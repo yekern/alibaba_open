17b0955faad0f8c3e3b4ba2a449b4e9399e2046a
WangFeng <yekern@163.com>
2024-06-13 13:42:13 +0800

    add 支付渠道

---
+++ src/tools/PayChannel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Yekern\AlibabaOpen\tools;
+
+class PayChannel
+{
+    //支付渠道 alipay(支付宝),shegou(诚e赊),kjpayV2(跨境宝)
+    const  ALIPAY = 'alipay';
+    const SHEGOU = 'shegou';
+
+    const KJPAYV2 = 'kjpayV2';
+}
